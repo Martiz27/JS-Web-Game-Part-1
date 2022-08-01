14621d4f3ed6784eb09e95ec36e2a9c38c7b7d4e
Charlie <ware.charlie914@gmail.com>
2022-07-31 22:23:08 -0700

    Fin

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(a,b,c) {
+    let object = document.createElement('img')
+    object.src = a
+    object.style.position = 'fixed'
+    object.style.left = b + 'px'
+    object.style.bottom = c + 'px'
+    document.body.append(object) 
+    return object
+}
+function newItem(a,b,c){
+    let object = document.createElement('img')
+    object.src = a
+    object.style.position = 'fixed'
+    object.style.left = b + 'px'
+    object.style.bottom = c + 'px'
+    document.body.append(object)
+    
+    object.addEventListener('click', function() {
+        object.remove()
+    })
+    
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage("assets/pine-tree.png", 450, 200)
+newItem("assets/sheild.png", 165, 185)
+newItem("assets/staff.png", 600, 100)
+newImage('assets/tree.png', 200, 300)
+newImage("assets/pillar.png", 350, 100)
+newImage("assets/crate.png", 150, 200)
+newImage("assets/well.png", 500, 425)
+newItem('assets/sword.png', 500, 405)
